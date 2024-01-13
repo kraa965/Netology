a8c051d338670910e9fdc25bc4f17f3b33931bce
kraa <kraa@omegafuture.ru>
2024-01-13 12:51:00 +0300

    fix

--- work2/filter/src/components/ProjectList.jsx
+++ work2/filter/src/components/ProjectList.jsx
@@ -1,6 +1,6 @@
 export const ProjectList = ({ projects }) => {
   return (
-    <div className={'grid grid-cols-3 gap-2'}>
+    <div className={'grid grid-cols-3 gap-2 items-center'}>
       {projects.map((item, index) => (
         <div key={index}>
           <img src={item.img} alt={item.category} className={'h-auto max-w-full '} />
